6c36697300186c61026d8d823727a4672e8f67eb
Shekhar373 <shekhar24102004@gmail.com>
2026-08-22 14:33:37 +0530

    hero text fix

--- src/Components/Homecomp/Hero.jsx
+++ src/Components/Homecomp/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="h-screen w-full bg-white relative ">
       <div className="h-screen w-full absolute top-[12%] lg:top-0 lg:flex justify-between lg:items-center p-5 text-[10vw] max-md:leading-[10vw] lg:text-[3vw]">
-        <div className="lg:tracking-tighter text-[10vw] max-md:leading-[10vw] flex flex-col lg:flex-row lg:gap-2">
+        <div className="lg:tracking-tighter max-sm:text-[10vw] max-md:leading-[10vw] flex flex-col lg:flex-row lg:gap-2">
           <h1>Creative</h1>
           <h1>Videos</h1>
         </div>
@@ -27,8 +27,8 @@ const Hero = () => {
         </h1>
       </div>
 
-      <div className="absolute bottom-[22%] p-5">
-        <h1 className="text-zinc-400 max-sm:text-sm lg:hidden flex">
+      <div className="absolute bottom-[21%] p-5">
+        <h1 className="text-zinc-400 max-sm:text-xs lg:hidden flex">
           Drag to explore the collection
         </h1>
       </div>
